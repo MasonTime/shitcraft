ca520a02ff4ab4bbd13a7f55a63800b1f4133b88
MasonTime <mason.jacobs101@gmail.com>
2022-01-28 12:25:03 -0600

    init

---
+++ main.lua
@@ -0,0 +1,26 @@
+--Main
+
+require"functions/collision"
+require "functions/graphics"
+require "objects/player"
+require "objects/block"
+
+function love.load()
+    sixteenth = love.graphics.getWidth()/16
+    tileset = love.graphics.newImage("graphics/tile.png")
+    tileset:setFilter("nearest","nearest")
+
+    blockLoad()
+    playerLoad()
+end
+
+function love.update(dt)
+    blockUpdate()
+    playerUpdate()
+end
+
+
+function love.draw()
+    blockDraw()
+    playerDraw()
+end
